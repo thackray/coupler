f2c768da4828e037b4d07ddb58dd878a1273953c
thackray <thackray@mit.edu>
2015-06-12 13:22:36 -0400

    Delete dum2.py

--- testing_run/dum2.py
+++
@@ -1,7 +0,0 @@
-# dummy model 2
-print "Dummy 2 is now running"
-input_stuff = open('input2.txt','r').read()
-print input_stuff
-print "Dummy 2 is done, stopping"
-with open('twoisdone.txt','w') as f:
-    f.write('Bye bye')
