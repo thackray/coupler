38999ac5a8f3278470fc48989dac5d4d0d26f6d1
thackray <thackray@mit.edu>
2015-06-21 19:41:24 -0400

    fixed some paths

--- mitgcmsetup.sh
+++ mitgcmsetup.sh
@@ -1,39 +1,39 @@
 #!/bin/csh
 
+set workingdir=/home/thackray/coupler/testing_run
+
 #setup script for mitgcm runs
 cd $workingdir
 mkdir verification/
 cd verification/
 
-mkdir run/mkdir global_pcb_llc90/ #(if you are running the PCB package)
+mkdir global_pcb_llc90/ #(if you are running the PCB package)
 cd global_pcb_llc90/
 mkdir run/
-set rundir $workingdir/verification/global_pcb_llc90/run
+set rundir=$workingdir/verification/global_pcb_llc90/run
 
 #6.1 Copy first batch of files
 #Copy these folders to verification/:
-cp /home/geos_harvard/yanxu/MITgcm/verification/global_oce_cs32 ../
-
-cp /home/geos_harvard/yanxu/MITgcm/verification/global_oce_input_fields ../
-/home/geos_harvard/yanxu/MITgcm/verification/global_pcb_llc90/input/
-/home/geos_harvard/yanxu/MITgcm/verification/global_pcb_llc90/input.core2
-/home/geos_harvard/yanxu/MITgcm/verification/global_pcb_llc90/input.ecco_v4
-/home/geos_harvard/yanxu/MITgcm/verification/global_pcb_llc90/input.ecmwf
-/home/geos_harvard/yanxu/MITgcm/verification/global_pcb_llc90/input_itXX
+cp -r /home/geos_harvard/yanxu/MITgcm/verification/global_oce_cs32 .
+cp -r /home/geos_harvard/yanxu/MITgcm/verification/global_oce_input_fields .
+cp -r /home/geos_harvard/yanxu/MITgcm/verification/global_oce_llc90/input .
+cp -r /home/geos_harvard/yanxu/MITgcm/verification/global_oce_llc90/input.core2 .
+cp -r /home/geos_harvard/yanxu/MITgcm/verification/global_oce_llc90/input.ecco_v4 .
+cp -r /home/geos_harvard/yanxu/MITgcm/verification/global_oce_llc90/input.ecmwf .
+cp -r /home/geos_harvard/yanxu/MITgcm/verification/global_oce_llc90/input_itXX .
 
 #Go into the input_itXX/ directory and update the file paths:
-set dirInputFields = $rundir/verification/global_oce_input_fields
-setdirLlc90 = /home/geos_harvard/yanxu/MITgcm/verification/global_ocea_llc90
+set dirInputFields=$rundir/verification/global_oce_input_fields
+set dirLlc90=/home/geos_harvard/yanxu/MITgcm/verification/global_ocea_llc90
 
 #Execute this command in your run/ directory.
 cd $rundir
 csh #(note: this command opens a c-shell)
-../input_itXX/prepare_run
+../input_itXX/prepare_run.sh
 exit #(note: this closes the c-shell)
 
 #6.2 Link forcing files to your run folder
 #Go to your run/ folder:
-cd $rundir
 ln -s ~gforget/ecco_v4_input/controls/* .
 ln -s ~gforget/ecco_v4_input/MITprof/* .
 ln -s ~gforget/ecco_v4_input/pickups/* .
@@ -51,7 +51,7 @@ mv runoff-2d-Fekete-1deg-mon-V4-SMOOTH.bin forcing/
 mkdir initial/
 #In this folder, you can put the initial conditions of your tracers. If you need a copy of the initial conditions for the ECCOv4 DARWIN
 #simulation, they're available here:
-cp /home/yanxu/MITgcm/verification/global_darwin_llc90/run1/initial/* initial/
+cp /home/geos_harvard/yanxu/MITgcm/verification/global_darwin_llc90/run1/initial/* initial/
 
 #6.5 Control files
 #Still in your run/ directory, make a control/ subdirectory:
