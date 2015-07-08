7e0be19f16de6516fd476f05a0addfe14d778e90
thackray <thackray@mit.edu>
2015-07-08 15:18:02 -0400

    fixed (?) seconds vs hours in stepping

--- models.py
+++ models.py
@@ -327,7 +327,7 @@ class MITgcm(Model):
                      '@HOURTIMESTEP':str(int(format_time(self.tend,
                                                  'MG',self.tstart))*3600),
                      '@PICKUPSTEP':str(int(format_time(self.tend,
-                                                 'MG',self.tstart))*3600),
+                                                    'MG',self.tstart)-1)*3600),
                      }
         if self.tstart==self.abststart:
             fill_dict['@PICKUPSUFF'] = '#'
