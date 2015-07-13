f2491c2f82d3b9f23bd03e962ca6b1dae8a56e43
thackray <thackray@mit.edu>
2015-07-13 15:50:37 -0400

    changed @HOURTIMESTEP to @DIAGTIMESTEP

--- models.py
+++ models.py
@@ -329,7 +329,7 @@ class MITgcm(Model):
                                                       'MG',self.abststart))+1),
                      '@TIMESTEP':str(int(format_time(self.tend,
                                                  'MG',self.tstart))),
-                     '@HOURTIMESTEP':str(int(format_time(self.tend,
+                     '@DIAGTIMESTEP':str(int(format_time(self.tend,
                                                  'MG',self.tstart))*3600),
                      '@PICKUPSTEP':str(int(format_time(self.tend,
                                                     'MG',self.tstart))*3600),
