f1271bf0facab3c49ca61ab3ab9b18c5610b4edd
thackray <thackray@mit.edu>
2015-07-17 08:39:29 -0400

    changed sharing to queue-holding jobs

--- sendgcm.sh
+++ sendgcm.sh
@@ -4,8 +4,16 @@ source /etc/profile.d/modules.csh
 
 module add matlab
 cd @RUNDIR
-rm MGsent
-touch MGsending
-matlab -nodesktop -nojvm -nodisplay -nosplash -r gcmtogeos -logfile gcmtogeos.log
-rm MGsending
-touch MGsent
+while ( ! -e STOP )
+if ( -e SEND ) then
+    rm SEND
+    rm MGsent
+    touch MGsending
+    matlab -nodesktop -nojvm -nodisplay -nosplash -r gcmtogeos -logfile gcmtogeos.log
+    rm MGsending
+    touch MGsent
+else
+    sleep 2
+endif
+end
+exit(0)
--- sendgeos.sh
+++ sendgeos.sh
@@ -1,11 +1,19 @@
-#!/bin/csh
-#PBS -q short
+#!/bin/csh -f
+#PBS -q xlong
 source /etc/profile.d/modules.csh
 
 module add matlab
 cd @RUNDIR
-rm GCsent
-touch GCsending
-matlab -nodesktop -nojvm -nodisplay -nosplash -r geostogcm -logfile geostogcm.log
-rm GCsending
-touch GCsent
+while ( ! -e STOP )
+if ( -e SEND ) then
+    rm SEND
+    rm GCsent
+    touch GCsending
+    matlab -nodesktop -nojvm -nodisplay -nosplash -r geostogcm -logfile geostogcm.log
+    rm GCsending
+    touch GCsent
+else
+    sleep 2
+endif
+end
+exit(0)
