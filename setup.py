6e9c2f7ad0d39ed0873d69711f244f80fb009bd7
thackray <thackray@mit.edu>
2015-09-24 13:00:24 -0400

    added --sandy setup

--- setup.py
+++ setup.py
@@ -2,22 +2,31 @@ from templates import FileTemplate
 import os
 import sys
 
-if len(sys.argv) < 3:
-    installdir = os.getcwd()
-else:
-    installdir = os.path.join(os.getcwd(),sys.argv[1])
-
+MODE = 'DEFAULT'
+installdir = os.getcwd()
+if len(sys.argv) > 2:
+    if sys.argv[2] in ['--sandy', '-sandy','-s']:
+        MODE = 'SANDY'
 
 files_to_install = ['geossetup.sh.template','mitgcmsetup.sh.template',
                     'example.py.template', 'prepare_run_ecco_v4.template',
                     'prepare_run_input.template','runonce.sh','qsub_itXX.csh',
                     'sendgcm.sh','sendgeos.sh','startup.sh.template',
-                    'subcoupler.sh']
+                    'subcoupler.sh','data.exch2.template']
+
 template_locations = [os.path.join('setup_templates',ff) for ff 
                     in files_to_install]
 
+if MODE == 'SANDY':
+    template_locations[6] = os.path.join('setup_templates',
+                                         'qsub_itXX.csh.template.sandy')
+    template_locations[1] = os.path.join('setup_templates',
+                                         'mitgcmsetup.sh.template.sandy')
+    template_locations[11] = os.path.join('setup_templates',
+                                         'data.exch2.template.sandy')
+
 for fi,loc in zip(files_to_install,template_locations):
-    T = FileTemplate(fi.rstrip('template').rstrip('.'))
+    T = FileTemplate(fi.split('.template')[0])
     T.load_template(loc)
     T.set_value('@INSTALLPATH',installdir)
     T.write()
